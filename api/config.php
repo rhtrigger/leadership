ec450558d4e90367ed29e0bf4e77b884b37b3276
Ryan Hopkins <ryan@triggermovement.com>
2017-01-22 17:46:25 +0000

    recommit digital files to leadership repo

---
+++ api/config.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'Cr5GmpNswXyUz6nkNrk8lhPFK');
+    define('CONSUMER_SECRET', 'zIveI6t9rgR3orLftZ52ump31fd1PXrfYFuXBXH2Eu9MVWHeSy');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '105528861-jisr2GtYF9Od01oFt8vLWZcrS2XhRuOWuaHTRouF');
+    define('ACCESS_SECRET', 'OijcOVKOiiI5HBODEwjp76PWhGk1D6A4Gaawflfdb3ofs');
